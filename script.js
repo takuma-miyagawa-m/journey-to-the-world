484ae48927cf2ee938e4d4ee293fe02478167169
takumaMi <a241064@matsu.shimane-u.ac.jp>
2026-02-23 15:46:35 -0300

    Publish site

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('loaded');
+const msg = document.getElementById("msg");
+const btn = document.getElementById("btn");
+btn.addEventListener("click", function() {
+  msg.textContent = "世界に羽ばたこう！";
+});
