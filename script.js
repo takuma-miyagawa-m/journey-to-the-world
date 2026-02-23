9843b30151d43472d61f5c976a27c2925e3aeb55
takumaMi <a241064@matsu.shimane-u.ac.jp>
2026-02-23 16:06:13 -0300

    add pink and birdpictures

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ const msg = document.getElementById("msg");
 const btn = document.getElementById("btn");
 btn.addEventListener("click", function() {
   msg.textContent = "世界に羽ばたこう！";
+  //page color change 
+    document.body.style.backgroundColor = "pink";
 });
