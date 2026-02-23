411d92c63dfd48bdb5b61a5b84e467896446bda1
takumaMi <a241064@matsu.shimane-u.ac.jp>
2026-02-23 16:23:06 -0300

    pop up bird pictures by pushing a bottun

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 console.log('loaded');
 const msg = document.getElementById("msg");
 const btn = document.getElementById("btn");
+const mypopover = document.getElementById("mypopover");
+
 btn.addEventListener("click", function() {
-  msg.textContent = "世界に羽ばたこう！";
-  //page color change 
+    msg.textContent = "世界に羽ばたこう！";
+    //page color change 
     document.body.style.backgroundColor = "pink";
+    //show images
+    mypopover.style.display = "block";
 });
